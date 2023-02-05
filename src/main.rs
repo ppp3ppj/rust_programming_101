6ef464f5a8504d28091f6c5bfe50e266d77ef5c9
ppp <ppp.3ppj@gmail.com>
2023-02-05 11:16:39 +0700

    stack memory learn and debug with gdb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// for demo code can use allow this = disble warn
+#![allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    let n1 = 1;
+    let n2 = 2;
+    ex1_1();
+    println!();
+    println!();
+}
+
+fn ex1_1() {
+    let n3 = 3;
+    ex1_2();
+    println!();
+}
+
+fn ex1_2() {
+    let n4 = 4;
+    println!();
 }
